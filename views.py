eb56105f000f161bdfe3ece98c8e6d9ccbb275c3
jess <jessgaming55@gmail.com>
2022-10-18 18:55:01 +0800

    inital commit

---
+++ views.py
@@ -0,0 +1,9 @@
+# views
+import flask as flk
+
+app = flk.Flask(__name__)
+app.config["SECRET_KEY"] = "5^6*4%#^Mf6jeW#95J9t25J36F%y3$i5"
+
+@app.route("/")
+def home():
+    return flk.render_template("index.html")
